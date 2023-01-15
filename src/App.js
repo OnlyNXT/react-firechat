70526bfc3e3dc4493088a5d4f443b579bc68711d
OnlyNXT <75180198+OnlyNXT@users.noreply.github.com>
2023-01-15 00:30:49 -0500

    Update App.js
    i've updated my API keys so the chatting app can work

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ import Channel from './components/Channel';
 import Loader from './components/Loader';
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_AUTH_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+   apiKey: "AIzaSyBsUeOyHYLl6lEQ-IHrnny-MQiQchdNT00",
+  authDomain: "nxtchat-a892a.firebaseapp.com",
+  projectId: "nxtchat-a892a",
+  storageBucket: "nxtchat-a892a.appspot.com",
+  messagingSenderId: "693140496745",
+  appId: "1:693140496745:web:d8290da6fd143eeb47ddf8",
+  measurementId: "G-9ED7JE7Q22"
 });
 
 const MoonIcon = props => (
